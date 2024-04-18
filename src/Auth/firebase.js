9323421475aa1c1aa63ce0f3a13d2aeea64bf30b
pavanpatel45 <158548230+pavanpatel45@users.noreply.github.com>
2024-04-18 16:56:40 +0530

    Update firebase.js

--- src/Auth/firebase.js
+++ src/Auth/firebase.js
@@ -7,13 +7,6 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD2q8MXkn8WJGuVR_vB6NgBSO6fnFABDSQ",
-  authDomain: "food-app-6da2f.firebaseapp.com",
-  projectId: "food-app-6da2f",
-  storageBucket: "food-app-6da2f.appspot.com",
-  messagingSenderId: "20206801866",
-  appId: "1:20206801866:web:b3f685a465425f3935f533",
-  measurementId: "G-NZ5YZFHNGH"
 };
 
 // Initialize Firebase
